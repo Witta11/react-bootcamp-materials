aa205c425bcad814d4efbe52c83f8743fa45d64f
Witta Glöckner <alina.glocke@gmail.com>
2021-02-09 14:13:24 +0100

    understanding import and exporting

---
+++ 04-create-react-app/my-first-react-app/src/food.js
@@ -0,0 +1,12 @@
+const fruits = [
+  "Banana",
+  "Pineapple",
+  "Mango",
+  "Coconut",
+  "Strawberries",
+  "Blueberries",
+  "Jackfruit",
+];
+
+export default fruits;
+// since its the only thing we are exporting
