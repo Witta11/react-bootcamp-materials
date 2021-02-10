bf74ba2ceeada52e37cfdb945f8fe4dff35a68ea
Witta Glöckner <alina.glocke@gmail.com>
2021-02-10 14:28:34 +0100

    understanding import export

---
+++ 04-create-react-app/my-first-react-app/src/hello.js
@@ -0,0 +1,11 @@
+class Hello extends Component {
+  render() {
+    return (
+      <div className="Hello">
+        <p className="Hello-text">Hi, this is so exiting!!</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
